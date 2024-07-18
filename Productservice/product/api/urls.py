a49855776a8ca3acc2a962430855d80d1ade9e56
rustamzada0 <rustemzade.tunay.2000@gmail.com>
2024-07-18 14:04:52 +0400

    ready!

--- Productservice/product/api/urls.py
+++ Productservice/product/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path('product/', views.ProductViews.as_view()),
+    path('product/generic/', views.ProductListCreateView.as_view()),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail')
 
 ]
--- Productservice/product/api/views.py
+++ Productservice/product/api/views.py
@@ -2,10 +2,12 @@ import requests
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+
 from ..models import Product
 from .serializers import ProductSerializer
 
-def check_access_token(access_token):
+def check_access_token(access_token): # utils.py -de saxlaya bilerik
     url = 'http://localhost:8000/account/api/token/verify/'
     # headers = {'Authorization': f'Bearer {access_token}'} lazim deyil cunki verify edende bodyde gonderirik
     response = requests.post(url, data={'token': access_token}) #headers=headers
@@ -26,7 +28,6 @@ class ProductViews(APIView):
     
         return Response(serializer.data)
 
-
     def post(self, request, *args, **kwargs):
         token = request.headers.get('Authorization')
         if not token:
@@ -44,49 +45,73 @@ class ProductViews(APIView):
             return Response({"detail": "Invalid token."}, status=status.HTTP_401_UNAUTHORIZED)
 
 
+class ProductDetailView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
 
-
-# class ProductListCreateView(generics.ListCreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         token = request.headers.get('Authorization')
-#         if not token:
-#             return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+    def put(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        token = request.headers.get('Authorization')
+        if not token:
+            return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
         
-#         token = token.split()[1]
-#         print(token)
+        token = token.split()[1]  # Bearer token_string'den token'ı alın
 
-#         if not check_access_token(token):
-#             return Response({"detail": "Invalid token."}, status=status.HTTP_401_UNAUTHORIZED)
+        if check_access_token(token):
+            print("Salam")
+            serializer = ProductSerializer(product, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"detail": "Invalid token."}, status=status.HTTP_401_UNAUTHORIZED)
 
-
-# class ProductListCreateView(generics.ListCreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-#     def create(self, request, *args, **kwargs):
-#         print(request.headers)
-#         print(request)
-#         token = request.headers.get('Authorization')
-#         print(token)
-#         if not token:
-#             return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+    def delete(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        token = request.headers.get('Authorization')
+        if not token:
+            return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
         
-#         token = token.split()[1] 
+        token = token.split()[1]  # Bearer token_string'den token'ı alın
 
-#         if not check_access_token(token):
-#             return Response({"detail": "Invalid token."}, status=status.HTTP_401_UNAUTHORIZED)
+        if check_access_token(token):
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"detail": "Invalid token."}, status=status.HTTP_401_UNAUTHORIZED)
 
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         headers = self.get_success_headers(serializer.data)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+# ########################## 2-ci version ########################## (GENERICS)
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import BasePermission
 
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
+class TokenRequiredMixin(BasePermission):
+
+    def has_permission(self, request, view):
+        token = request.headers.get('Authorization')
+        if not token:
+            self.message = "Authentication credentials were not provided."
+            return False
+        
+        token = token.split()[1]  # Bearer token_string'den token'ı alın
+
+        if not check_access_token(token):
+            self.message = "Invalid token."
+            return False
+
+        return True
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [TokenRequiredMixin]
+
+
+
+
 
 
